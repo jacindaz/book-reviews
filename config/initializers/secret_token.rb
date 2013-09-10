1d67c2ef98583b4be874d7e14d1aacd7ae1be7fe
Adam Sheehan <a.t.sheehan@gmail.com>
2013-09-10 13:42:45 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookReviews::Application.config.secret_key_base = '6e015deeed9b242c6599f71c5def094358278fa5b1add3a8a9624da9ffe4efebd6554bae9c405c4fd23586851477f743ea7f0c8af2c22a0271d4ad732fca56d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BookReviews::Application.config.session_store :cookie_store, key: '_book_reviews_session'
