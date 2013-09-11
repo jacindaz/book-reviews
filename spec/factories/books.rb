e9ec85b6af9e8e2b76986acf86e38104a9cda3e4
Adam Sheehan <a.t.sheehan@gmail.com>
2013-09-11 10:20:28 -0400

    Move factories to a single file.

--- spec/factories/books.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :book do
-    title "MyString"
-    published "2013-09-10 13:52:44"
-    author_id 1
-  end
-end
