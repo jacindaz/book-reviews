21b1208b1f46d3ee69654fd7a8fac4f09b6357d6
Adam Sheehan <a.t.sheehan@gmail.com>
2014-06-23 23:23:48 -0400

    Update rspec.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130910175339) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: true do |t|
     t.string   "name",       null: false
     t.datetime "created_at"
